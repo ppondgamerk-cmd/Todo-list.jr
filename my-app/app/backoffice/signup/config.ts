1453a0b7b1683e32002353a077ff83ad136c5f66
ppondgamerk <ppondgamerk@example.com>
2026-08-12 03:08:10 +0700

    chore: update production API fallback domain and local environment config

--- my-app/app/backoffice/signup/config.ts
+++ my-app/app/backoffice/signup/config.ts
@@ -1,3 +1,3 @@
 export const Config = {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
+    apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://todo-list-jr-iota.vercel.app'
 }
